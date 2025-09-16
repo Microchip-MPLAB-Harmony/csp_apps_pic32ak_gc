f07a00250dd65a99b93a9eeb3f87fdf2b9313292
Syam Nair <Syam.Nair@microchip.com>
2025-09-16 17:58:29 +0000

    [APP] Regenerated demo for timer__timer_interrupt
    MH3C-261

--- apps/timer/timer_interrupt/src/config/default/peripheral/tmr/plib_tmr1.h
+++ apps/timer/timer_interrupt/src/config/default/peripheral/tmr/plib_tmr1.h
@@ -50,7 +50,7 @@
 
 #define TIMER_CLOCK_FREQUENCY          4000000
 
-#define TMR_INTERRUPT_PERIOD_IN_US     250000
+#define TMR_INTERRUPT_PERIOD_IN_NS     250000000
 typedef struct
 {
     uint32_t start;
